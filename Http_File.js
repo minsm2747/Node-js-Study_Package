8c9e848c135fa22d6e30aaefca5f56c8c2b8986a
DESKTOP-A20E83D\minseok <su960618@gmail.com>
2020-06-22 12:11:14 +0900

    Node.js 실습 코드 GitHub 2차 업로드

---
+++ Http_File.js
@@ -0,0 +1,10 @@
+// 모듈 추출
+var fs = require('fs');
+var http = require('http');
+
+// 웹 서버 생성 및 실행
+http.createServer(function (request, response) {
+
+}).listen(52273, function () {
+    console.log('Server Running at http://127.0.0.1:52273');
+});
